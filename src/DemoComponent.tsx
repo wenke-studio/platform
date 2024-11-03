ed7e76b42a4031a4535624517922dcba8e6d64bf
wenke.lai <wenke.studio@gmail.com>
2024-11-03 21:51:07 +0800

    build: import zustand package

--- src/DemoComponent.tsx
+++ src/DemoComponent.tsx
@@ -1,5 +1,19 @@
+import useBearStore from "@/store";
+
+const Controls = () => {
+  const increase = useBearStore((state) => state.increase);
+  return <button onClick={() => increase(1)}>one up</button>;
+};
+
 const DemoComponent = () => {
-  return <div>DemoComponent</div>;
+  const bears = useBearStore((state) => state.bears);
+
+  return (
+    <div className="flex row gap-4">
+      <h1>{bears} around here...</h1>
+      <Controls />
+    </div>
+  );
 };
 
 export default DemoComponent;
