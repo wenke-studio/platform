72b7f8c873a655ff55964796779dd9ba28b89b4a
wenke.lai <wenke.studio@gmail.com>
2024-11-12 16:50:46 +0800

    build: import framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import DemoComponent from "@/DemoComponent";
 import { IconArrowLeft } from "@tabler/icons-react";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+
+const DemoAction = () => {
+  return (
+    <motion.div
+      className="w-[20px] h-[20px] bg-red-500"
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+    />
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -37,6 +58,9 @@ function App() {
           <p>{import.meta.env.VITE_PROJECT_VERSION}</p>
         </div>
         <DemoComponent />
+        <div>
+          <DemoAction />
+        </div>
       </div>
     </>
   );
