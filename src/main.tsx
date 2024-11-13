6b6f4646034f5a54d171ccf882cbeb807ae59dfa
wenke.lai <wenke.studio@gmail.com>
2024-11-13 14:35:28 +0800

    refactor: rename index.css to global.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme/theme-provider.tsx";
-import "./index.css";
+import "./global.css";
 import router from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
